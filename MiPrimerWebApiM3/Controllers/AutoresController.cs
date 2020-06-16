8c930da508ffa3682b46edbb3b8026e639b3ca1e
Your Name <you@example.com>
2020-06-16 17:46:17 -0300

    configuracion para el primer get

---
+++ MiPrimerWebApiM3/Controllers/AutoresController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiPrimerWebApiM3.Contexts;
+using MiPrimerWebApiM3.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiPrimerWebApiM3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Autor>> Get()
+        {
+            return context.Autores.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Autor> Get(int id)
+        {
+            var autor = context.Autores.FirstOrDefault(x=> x.Id  == id);
+
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            return autor;
+        }
+
+        
+
+    }
+}
